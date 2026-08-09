9d3b17923252574d420f19c74b27c1e5b839dee7
MelnCat <melncatuwu@gmail.com>
2026-08-09 16:10:34 -0700

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,29 +8,27 @@ import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-    site: "https://melncat.github.io",
+	site: "https://melncat.github.io",
 
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.google(),
-                name: "Roboto",
-                cssVariable: "--font-roboto",
-                weights: [400, 700]
-            },
-            {
-                provider: fontProviders.google(),
-                name: "Cascadia Code",
-                cssVariable: "--font-cascadia-code"
-            },
-            {
-                provider: fontProviders.google(),
-                name: "Fira Code",
-                cssVariable: "--font-fira-code",
-                weights: [400, 700]
-            },
-        ],
-    },
+	fonts: [
+		{
+			provider: fontProviders.google(),
+			name: "Roboto",
+			cssVariable: "--font-roboto",
+			weights: [400, 700],
+		},
+		{
+			provider: fontProviders.google(),
+			name: "Cascadia Code",
+			cssVariable: "--font-cascadia-code",
+		},
+		{
+			provider: fontProviders.google(),
+			name: "Fira Code",
+			cssVariable: "--font-fira-code",
+			weights: [400, 700],
+		},
+	],
 
-    integrations: [mdx(), react(), svelte()],
+	integrations: [mdx(), react(), svelte()],
 });
