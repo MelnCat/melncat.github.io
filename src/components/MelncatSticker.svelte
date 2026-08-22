f8c255af7f5fe89e3f27a27eedebd51025605b8c
MelnCat <melncatuwu@gmail.com>
2026-08-22 13:03:58 -0700

    rainbow

--- src/components/MelncatSticker.svelte
+++ src/components/MelncatSticker.svelte
@@ -55,28 +55,55 @@
 				y2={0.5 + rot.current.y * 0.2 + 0.35}
 			>
 				<stop offset="30%" stop-color="white" stop-opacity="0" />
-				<stop offset="50%" stop-color="white" stop-opacity={0.65} />
+				<stop offset="50%" stop-color="white" stop-opacity="1" />
 				<stop offset="70%" stop-color="white" stop-opacity="0" />
 			</linearGradient>
 
 			<mask id="shineMask" maskUnits="userSpaceOnUse" x="0" y="0" width="1250" height="400">
 				<text x="20" y="10" class="main" dominant-baseline="hanging">melncat</text>
 			</mask>
+
+			<linearGradient
+				id="rainbow"
+				gradientUnits="objectBoundingBox"
+				x1={-0.5 + rot.current.x * -0.25}
+				y1={0.5 + rot.current.y * -0.25}
+				x2={-1 + rot.current.x * -0.25}
+				y2={0.8 + rot.current.y * -0.25}
+				spreadMethod="repeat"
+			>
+				<stop offset="0%" stop-color="#ff0000" />
+				<stop offset="16%" stop-color="#ff6600" />
+				<stop offset="32%" stop-color="#ffff00" />
+				<stop offset="48%" stop-color="#00ff88" />
+				<stop offset="64%" stop-color="#00ccff" />
+				<stop offset="80%" stop-color="#6600ff" />
+				<stop offset="96%" stop-color="#ff00cc" />
+				<stop offset="100%" stop-color="#ff0000" />
+			</linearGradient>
 		</defs>
 		<text x="20" y="10" class="main" dominant-baseline="hanging">melncat</text>
-		<rect x="20" y="10" width="1250" height="400" fill="url(#shine)" mask="url(#shineMask)" />
+		<rect width="1250" height="400" fill="url(#rainbow)" mask="url(#shineMask)" opacity="0.5" class="rainbow" />
+		<rect width="1250" height="400" fill="url(#shine)" mask="url(#shineMask)" opacity="0.7" class="shine" />
 	</svg>
 </div>
 
 <style>
 	.sticker {
+		width: 30em;
 		font-family: var(--font-strichpunkt-sans);
+	}
+	.shine {
+		mix-blend-mode: hard-light;
+	}
+	.rainbow {
+		mix-blend-mode: multiply;
 	}
 	svg {
 		overflow: visible;
 	}
 	.main {
-		fill: #257523;
+		fill: #1c611b;
 	}
 	text {
 		font-size: 16em;
